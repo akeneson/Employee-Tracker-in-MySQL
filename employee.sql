54ab8ced78ec07d9e6583492202bd225c0730fea
Amy Keneson <amy.l.keneson@icloud.com>
2020-09-22 17:51:38 -0500

    sql files merged

--- employee.sql
+++ employee.sql
@@ -4,15 +4,35 @@ CREATE database employeeDB;
 
 USE employeeDB;
 
-CREATE TABLE roles (
-  id INT NOT NULL,
-  first_name VARCHAR(30) NULL,
-  last_name VARCHAR(30) NULL,
+CREATE TABLE employee (
+  ID INT NOT NULL,
+  First_name VARCHAR(30) NULL,
+  Last_name VARCHAR(30) NULL,
 --  to hold reference to role employee has
-  role_id INT NOT NULL,
+  Role_id INT NOT NULL,
 --  to hold reference to another employee that manager of the current employee.
-  manager_id INT NULL,
+  Manager_id INT NULL,
   PRIMARY KEY (id)
 );
 
-SELECT * FROM employeeDB;
+CREATE TABLE department (
+  ID INT NOT NULL,
+  -- to hold department name
+  Name VARCHAR(30) NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE roles (
+  ID INT NOT NULL,
+  -- to hold role title
+  Title VARCHAR(30) NULL,
+  -- to hold role salary
+  Salary DECIMAL(65) NULL,
+  -- to hold reference to department role belongs to
+  Department_id INT NULL,
+  PRIMARY KEY (id)
+);
+
+SELECT * FROM roles;
+SELECT * FROM employee;
+SELECT * FROM department;
