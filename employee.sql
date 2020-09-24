7886ce90f18cd2bc2b5c5aea6fd260da87e24f76
Amy Keneson <amy.l.keneson@icloud.com>
2020-09-23 19:21:10 -0500

    add department function

--- employee.sql
+++ employee.sql
@@ -5,33 +5,43 @@ CREATE database employeeDB;
 USE employeeDB;
 
 CREATE TABLE employee (
-  ID INT NOT NULL,
-  First_name VARCHAR(30) NULL,
-  Last_name VARCHAR(30) NULL,
+  employee_ID INT NOT NULL,
+  First_name VARCHAR(30),
+  Last_name VARCHAR(30),
 --  to hold reference to role employee has
   Role_id INT NOT NULL,
 --  to hold reference to another employee that manager of the current employee.
   Manager_id INT NULL,
-  PRIMARY KEY (id)
+  PRIMARY KEY (employee_ID)
 );
 
 CREATE TABLE department (
-  ID INT NOT NULL,
+  department_ID INT NOT NULL AUTO_INCREMENT,
   -- to hold department name
-  Name VARCHAR(30) NULL,
-  PRIMARY KEY (id)
+  Department_name VARCHAR(30),
+  PRIMARY KEY (department_ID)
 );
 
 CREATE TABLE roles (
-  ID INT NOT NULL,
+  role_ID INT NOT NULL,
   -- to hold role title
   Title VARCHAR(30) NULL,
   -- to hold role salary
   Salary DECIMAL(65) NULL,
   -- to hold reference to department role belongs to
-  Department_id INT NULL,
-  PRIMARY KEY (id)
+  department_ID INT NULL,
+  PRIMARY KEY (role_ID)
 );
+
+INSERT INTO employee (employee_ID, First_name, Last_name, Role_id, Manager_id)
+VALUES ("1","Amy", "K", "1", "5");
+
+INSERT INTO department (department_ID, Department_name)
+VALUES("1", "Alpha");
+
+INSERT INTO roles(role_ID, Title, Salary, department_ID)
+VALUES("10","Student", "25", "1");
+
 
 SELECT * FROM roles;
 SELECT * FROM employee;
--- employeeTracker.js
+++ employeeTracker.js
@@ -18,7 +18,7 @@ connection.connect(function(err){
 
 function runInquirer(){
     inquirer.prompt({
-        name: "Main",
+        name: "action",
         type: "rawlist",
         message: "Main Menu: Please choose from the following: ",
         choices: [
@@ -33,26 +33,28 @@ function runInquirer(){
     }).then(function(answer){
         console.log(answer);
         switch(answer.action){
-            case "Add departments, roles, or employee":
-                addOption();
-            case "View departments, roles, or employee":
-                viewOption();
-                break;
-            case "Update employee role":
-                updateRole();
-                break;
-            case "Update employee manager":
-                updateManager();
-                break;
-            case "View employees by manager":
-                viewManager();
-                break;
-            case "Delete department, roles, or employee":
-                deleteOption();
-                break;
-            case "View the total utilized budget of a department":
-                budget();
-                break;
+        case "Add departments, roles, or employee":
+            console.log("actionOption chosen");
+            addOption();
+            break;
+        case "View departments, roles, or employee":
+            viewOption();
+            break;
+        case "Update employee role":
+            updateRole();
+            break;
+        case "Update employee manager":
+            updateManager();
+            break;
+        case "View employees by manager":
+            viewManager();
+            break;
+        case "Delete department, roles, or employee":
+            deleteOption();
+            break;
+        case "View the total utilized budget of a department":
+            budget();
+            break;
         }
     })
 }
@@ -61,8 +63,8 @@ function addOption(){
     console.log("addOption function called");
     inquirer.
         prompt({
-            name:"addOption",
-            type: "choices",
+            name: "action",
+            type: "rawlist",
             message: "Which would you like to add?",
             choices: [
                 "Add department",
@@ -70,20 +72,79 @@ function addOption(){
                 "Add employee"
             ]            
     })
-    .then (function(res){
-        console.log(res);
-        if (res == "Add department") {
-        inquirer.prompt({
-                name:"departmentName",
-                type:"input",
-                message: "What is the name of the new department?"
-            }).then(function(res){
-                console.log(res);
-                connection.query("INSERT INTO department SET ?", {name: res.name},
-                function(err, res){
-                    if(err) throw err;
-                });
-            });
+    .then (function(answer){
+        console.log(answer);
+        switch(answer.action){
+            case "Add department":
+                console.log("Add departments selected");
+                addDepartment();
+                break;
+            case "Add roles":
+                console.log("Add roles selected");
+                addRoles();
+                break;
+            case "Add employee":
+                console.log("Add employee selected");
+                addEmployee();
+                break;
+            case "Return to previous menu":
+                runInquirer();
+                break;
         }
     });
 }
+
+function addDepartment(){
+    console.log("addDepartment function called");
+    inquirer.prompt({
+        type: "input",
+        message: "Enter the name of the department: ",
+        name:"deptName"
+    }).then(function(response){
+        console.log(response);
+        connection.query("INSERT INTO department SET ?", { Department_name: response.name },
+        function(error,response){
+        if (error) throw error;
+    });
+    })
+}
+
+// function addEmployee(){
+//     console.log("addEmployee function called");
+//     inquirer.prompt([
+//     {
+//         type: "input",
+//         message: "Enter the first name of the employee: ",
+//         name:"firstName"
+//     },
+//     {
+//         type: "input",
+//         message: "Enter the first name of the employee: ",
+//         name:"lastName"
+//     },
+//     {
+//         type: "list",
+//         message: "What's is the employee's title?",
+//         name: "employeeTitle",
+//         choices: showRoles
+//     },
+//     {
+//         type: "list",
+//         message: "Who is the employee's manager?",
+//         name: "employeeManager",
+//         choices: showManger
+//     }
+//     ]).then(function(response){
+//         console.log(response);
+//         connection.query("INSERT INTO employee SET ?", { 
+//             First_name: response.first_name,
+//             Last_name: response.last_name,
+//             Fole_id: response.employeeTitle,
+//             Manager_id: response.employeeManager 
+//          },
+//         function(error,response){
+//         if (error) throw error;
+//     });
+//     })
+//     runInquirer();
+// }
